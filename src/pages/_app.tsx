a0c8d84f60290331b3421e7efbefe61b4e6eefae
Isaac <isaac.castro.dev@gmail.com>
2023-06-19 14:38:08 -0600

    ChakraUI setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// ** NextJS
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+// ** ChakraUI
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
