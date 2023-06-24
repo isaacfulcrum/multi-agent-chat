99e86acc8da86958840ce3eb9bba191976eb207c
Isaac <isaac.castro.dev@gmail.com>
2023-06-24 02:29:07 -0600

    Implemented message streaming completion

--- src/chat/api.ts
+++ src/chat/api.ts
@@ -1,22 +1,99 @@
 // NOTE: This shouldn't be used in the client, only in the server
 // For demo purposes, we're using it in the client
-import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+import {
+  ChatCompletionRequestMessage,
+  Configuration,
+  CreateChatCompletionRequest,
+  OpenAIApi,
+} from "openai";
+import { BehaviorSubject, Subject } from "rxjs";
 
 // ********************************************************************************
 const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+const OPENAI_CHAT_API = "https://api.openai.com/v1/chat/completions";
 
-// This function sends the messages to OpenAI and returns the response as a string
-export const getChatCompletion = async (messages: ChatCompletionRequestMessage[]): Promise<string> => {
-  const { data } = await openai.createChatCompletion({
-    model: "gpt-4",
-    messages,
-    max_tokens: 1000,
-    temperature: 0.7,
-  });
-  const message = data.choices[0].message?.content;
-  if(!message) throw new Error('No message returned from OpenAI')
-  return message;
+// Subject indicating the stream of messages sent to the subscribers
+// NOTE: we use a BehaviorSubject so the subscribers get the last value when they subscribe
+const chatCompletionSubject = new BehaviorSubject<string>("");
+let currentMessage = "";
+const updateMessage = (newContent: string) => {
+  currentMessage += newContent;
+  chatCompletionSubject.next(currentMessage);
+};
+
+const resetMessage = () => {
+  currentMessage = "";
+  chatCompletionSubject.next(currentMessage);
+};
+
+export const getChatCompletion = (): Subject<string> => {
+  return chatCompletionSubject;
+};
+
+export const fetchChatCompletion = async (
+  messages: ChatCompletionRequestMessage[]
+): Promise<void> => {
+  try {
+    const specs: CreateChatCompletionRequest = {
+      model: "gpt-4",
+      messages,
+      max_tokens: 600,
+      stream: true, // For streaming responses
+    };
+
+    // Fetch the response from the OpenAI API with the signal from AbortController
+    const response = await fetch(OPENAI_CHAT_API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify(specs),
+    });
+
+    if (!response.body) {
+      throw new Error("Network response was not ok");
+    }
+
+    // Read the response as a stream of data
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder("utf-8");
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        // Stream finished
+        break;
+      }
+      // Massage and parse the chunk of data
+      const chunk = decoder.decode(value);
+      const lines = chunk
+        .toString()
+        .split("\n")
+        .filter((line) => line.trim() !== "");
+      for (const line of lines) {
+        const message = line.replace(/^data: /, "");
+        if (message === "[DONE]") {
+          resetMessage();
+          return; // Stream finished
+        }
+        try {
+          const parsed = JSON.parse(message);
+          const { choices } = parsed;
+        const { delta } = choices[0];
+        const { content } = delta;
+          if (content !== undefined) {
+            updateMessage(parsed.choices[0].delta?.content);
+          }
+        } catch (error) {
+          console.error("Could not JSON parse stream message", message, error);
+        }
+      }
+    }
+  } catch (error) {
+    chatCompletionSubject.error(error);
+  }
 };
--- src/chat/service.ts
+++ src/chat/service.ts
@@ -3,8 +3,13 @@ import { nanoid } from "nanoid";
 
 import { agentServiceInstance } from "@/agent/service";
 
-import { getChatCompletion } from "./api";
-import { chatMessageToCompletionMessage, AssistantChatMessage, ChatMessage, ChatMessageRoleEnum } from "./type";
+import { fetchChatCompletion, getChatCompletion } from "./api";
+import {
+  chatMessageToCompletionMessage,
+  AssistantChatMessage,
+  ChatMessage,
+  ChatMessageRoleEnum,
+} from "./type";
 
 // ********************************************************************************
 export class ChatService {
@@ -15,23 +20,35 @@ export class ChatService {
   }
 
   // ------------------------------------------------------------------------------
-  public messages: ChatMessage[] = [];  
+  public messages: ChatMessage[] = [];
 
   /** stream of updated messages sent to the subscribers */
   // NOTE: we use a BehaviorSubject so the subscribers get the last value when they subscribe
   public readonly onMessageUpdates$: BehaviorSubject<ChatMessage[]>;
-  
+
   /** indicates if the Completion is being run */
   public isLoading: boolean = false;
-  
+
+  private currentCompletionMessageId?: string;
+  private completionMessage: string = "";
+
   // == Lifecycle =================================================================
   protected constructor() {
     this.onMessageUpdates$ = new BehaviorSubject(this.messages);
+    // Subscribe to the chat completion stream
+    getChatCompletion().subscribe({
+      next: (content) => {
+        this.completionMessage = content;
+        if (this.currentCompletionMessageId && content) {
+          this.updateMessage(this.currentCompletionMessageId, content);
+        }
+      },
+    });
   }
 
   // == Private Methods ===========================================================
   public async runCompletion() {
-    if(this.isLoading) return;
+    if (this.isLoading) return;
 
     const agent = agentServiceInstance.currentAgent;
     // Format messages as OpenAI expects them
@@ -39,24 +56,51 @@ export class ChatService {
 
     // If there's a current agent, we add its description as a system message
     // NOTE: add to the start of the array so it's the first message
-    if (agent) formatedMessages.unshift({ role: ChatMessageRoleEnum.System, content: agent.description, });
+    if (agent)
+      formatedMessages.unshift({ role: ChatMessageRoleEnum.System, content: agent.description });
 
     // Send new message the chat
     try {
       this.isLoading = true;
-      const content = await getChatCompletion(formatedMessages),
-      id = nanoid();
+
+      fetchChatCompletion(formatedMessages);
+      const id = nanoid();
       let message: AssistantChatMessage;
       // If there's a current agent, we add it to the message
-      if (agent) message = { id, role: ChatMessageRoleEnum.Assistant, content, isAgent: true, agent, };
-      else message = { id, role: ChatMessageRoleEnum.Assistant, content, isAgent: false, };
-
+      if (agent)
+        message = {
+          id,
+          role: ChatMessageRoleEnum.Assistant,
+          content: this.completionMessage,
+          isAgent: true,
+          agent,
+        };
+      else
+        message = {
+          id,
+          role: ChatMessageRoleEnum.Assistant,
+          content: this.completionMessage,
+          isAgent: false,
+        };
       this.addMessage(message);
+      this.currentCompletionMessageId = id;
     } catch (error) {
       console.error(error);
-    } finally { 
+    } finally {
       this.isLoading = false;
     }
+  }
+
+  private async updateMessage(chatMessageId: string, content: string) {
+    // Find the message in the array
+    const index = this.messages.findIndex((message) => message.id === chatMessageId);
+    if (index === -1) return;
+    // place the new message in the array
+    console.log("im here");
+    this.messages[index] = { ...this.messages[index], content };
+    this.messages = [...this.messages];
+    // Emit the new messages to the subscribers
+    this.onMessageUpdates$.next(this.messages);
   }
 
   // == Public Methods ============================================================
