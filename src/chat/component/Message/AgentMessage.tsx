8700c74861b5b47c325e4fcfecec64c720918bba
Isaac <isaac.castro.dev@gmail.com>
2023-06-26 19:17:15 -0600

    Implemented edit and delete handlers on messages

--- src/chat/component/Message/AgentMessage.tsx
+++ src/chat/component/Message/AgentMessage.tsx
@@ -1,12 +1,25 @@
-import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Stack, Text, useDisclosure } from "@chakra-ui/react";
 
-import { getContrastColor } from "@/utils/colors";
+import { chatServiceInstance } from "@/chat/service";
+
 import { AssistantChatMessage } from "@/chat/type";
+import { getContrastColor } from "@/utils/colors";
+
+import { DeleteButton, EditButton } from "../Button";
+import { EditModal } from "./EditModal";
 
 // ********************************************************************************
 export const AgentMessage: React.FC<AssistantChatMessage> = (message) => {
   const color = message.isAgent ? message.agent.color : "teal";
   const contrastColor = message.isAgent ? getContrastColor(message.agent.color) : "white";
+
+  // == Hook ======================================================================
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  // == Handler ===================================================================
+  const handleDelete = () => chatServiceInstance.removeMessage(message.id);
+  const handleEdit = () => onOpen();
+
   return (
     <Box maxWidth="90%">
       <Flex gap="1em" px="6" py="2">
@@ -15,16 +28,23 @@ export const AgentMessage: React.FC<AssistantChatMessage> = (message) => {
           backgroundColor={color}
           color={contrastColor}
         />
-        <Box
-          backgroundColor={color}
-          color={contrastColor}
-          padding="4"
-          boxShadow="lg"
-          borderRadius="lg"
-        >
-          <Text>{message.content}</Text>
-        </Box>
+        <Stack>
+          <Box
+            backgroundColor={color}
+            color={contrastColor}
+            padding="4"
+            boxShadow="lg"
+            borderRadius="lg"
+          >
+            <Text>{message.content}</Text>
+          </Box>
+          <Flex width="100%" gap="0.5em" justify="flex-start">
+            <EditButton onClick={handleEdit} />
+            <DeleteButton onClick={handleDelete} />
+          </Flex>
+        </Stack>
       </Flex>
+      <EditModal message={message} isOpen={isOpen} onClose={onClose} />
     </Box>
   );
 };
--- src/chat/component/Message/UserMessage.tsx
+++ src/chat/component/Message/UserMessage.tsx
@@ -1,21 +1,39 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Stack, Text, useDisclosure } from "@chakra-ui/react";
+
+import { chatServiceInstance } from "@/chat/service";
 
 import { UserChatMessage } from "@/chat/type";
 
+import { DeleteButton, EditButton } from "../Button";
+import { EditModal } from "./EditModal";
+
 // ********************************************************************************
-export const UserMessage: React.FC<UserChatMessage> = ({ content }) => {
+export const UserMessage: React.FC<UserChatMessage> = (message) => {
+  // == Hook ======================================================================
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  // == Handler ===================================================================
+  const handleDelete = () => chatServiceInstance.removeMessage(message.id);
+  const handleEdit = () => onOpen();
+
   return (
     <Flex gap="1em" px="6" py="2" justify="end">
-      <Box
-        backgroundColor="teal"
-        color="white"
-        padding="4"
-        boxShadow="lg"
-        borderRadius="lg"
-        maxWidth="90%"
-      >
-        <Text textAlign="right">{content}</Text>
-      </Box>
+      <Stack  maxWidth="90%">
+        <Box
+          backgroundColor="teal"
+          color="white"
+          padding="4"
+          boxShadow="lg"
+          borderRadius="lg"
+        >
+          <Text textAlign="right">{message.content}</Text>
+        </Box>
+        <Flex width="100%" gap="0.5em" justify="flex-end">
+          <EditButton onClick={handleEdit} />
+          <DeleteButton onClick={handleDelete} />
+        </Flex>
+      </Stack>
+      <EditModal message={message} isOpen={isOpen} onClose={onClose} />
     </Flex>
   );
 };
