96156c855a5126459e72a15f326e12f27873259f
Isaac <isaac.castro.dev@gmail.com>
2023-06-26 19:37:55 -0600

    Only display action buttons on hovering a message

--- src/chat/component/Message/AgentMessage.tsx
+++ src/chat/component/Message/AgentMessage.tsx
@@ -21,7 +21,7 @@ export const AgentMessage: React.FC<AssistantChatMessage> = (message) => {
   const handleEdit = () => onOpen();
 
   return (
-    <Box maxWidth="90%">
+    <Box maxWidth="90%" role="group">
       <Flex gap="1em" px="6" py="2">
         <Avatar
           name={message.isAgent ? message.agent.name : "Chatbot"}
@@ -29,18 +29,14 @@ export const AgentMessage: React.FC<AssistantChatMessage> = (message) => {
           color={contrastColor}
         />
         <Stack>
-          <Box
-            backgroundColor={color}
-            color={contrastColor}
-            padding="4"
-            boxShadow="lg"
-            borderRadius="lg"
-          >
+          <Box backgroundColor={color} color={contrastColor} padding="4" boxShadow="lg" borderRadius="lg">
             <Text>{message.content}</Text>
           </Box>
-          <Flex width="100%" gap="0.5em" justify="flex-start">
-            <EditButton onClick={handleEdit} />
-            <DeleteButton onClick={handleDelete} />
+          <Flex width="100%" justify="flex-end" minH="1.5em">
+            <Flex gap="0.5em" display="none" _groupHover={{ display: "flex" }}>
+              <EditButton onClick={handleEdit} />
+              <DeleteButton onClick={handleDelete} />
+            </Flex>
           </Flex>
         </Stack>
       </Flex>
--- src/chat/component/Message/UserMessage.tsx
+++ src/chat/component/Message/UserMessage.tsx
@@ -17,20 +17,16 @@ export const UserMessage: React.FC<UserChatMessage> = (message) => {
   const handleEdit = () => onOpen();
 
   return (
-    <Flex gap="1em" px="6" py="2" justify="end">
-      <Stack  maxWidth="90%">
-        <Box
-          backgroundColor="teal"
-          color="white"
-          padding="4"
-          boxShadow="lg"
-          borderRadius="lg"
-        >
+    <Flex gap="1em" px="6" pt="2" justify="end" role="group">
+      <Stack maxWidth="90%">
+        <Box backgroundColor="teal" color="white" padding="4" boxShadow="lg" borderRadius="lg">
           <Text textAlign="right">{message.content}</Text>
         </Box>
-        <Flex width="100%" gap="0.5em" justify="flex-end">
-          <EditButton onClick={handleEdit} />
-          <DeleteButton onClick={handleDelete} />
+        <Flex width="100%" justify="flex-start" minH="1.5em">
+          <Flex gap="0.5em" display="none" _groupHover={{ display: "flex" }}>
+            <EditButton onClick={handleEdit} />
+            <DeleteButton onClick={handleDelete} />
+          </Flex>
         </Flex>
       </Stack>
       <EditModal message={message} isOpen={isOpen} onClose={onClose} />
