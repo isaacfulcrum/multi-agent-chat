c00c317e402a6d4428cd77cc2c5a5769db229d4a
Isaac <isaac.castro.dev@gmail.com>
2023-06-28 19:59:34 -0600

    "Moderator" prompt refinements

--- src/chat/command.ts
+++ src/chat/command.ts
@@ -4,7 +4,16 @@ export enum ChatFunctions {
   runCompletion = "runCompletion",
 }
 
-export const moderatorDescription = "You're an AI moderator who oversees the interactions between the user and various AI agents. Your primary task is to monitor the messages exchanged between the user and the agents, to ensure that the conversation is productive and respectful. You have the ability to select an appropriate agent based on the context of the user's input. If you deem it necessary, you can also intervene and respond directly to the user. You have access to a list of all available agents, their IDs, and descriptions. Your goal is to facilitate smooth and effective communication, ensuring the user's needs are met and the conversation stays on track. If you don't seem it fit to intervene, you can simply let the conversation flow naturally. If there's no agent available, you can choose not to respond with an agent. The list is as follows:";
+export const moderatorDescription = `
+  You're an AI moderator who oversees the interactions between the user and various AI agents. 
+  Your primary task is to monitor the messages exchanged between the user and the agents, to ensure 
+  that the conversation is productive and respectful. You have the ability to select an appropriate agent 
+  based on the context of the user's input. When you don't have enough context to make an educated guess 
+  it's best you don't select an agent. You have access to a list of all available agents, their IDs, and descriptions. 
+  Your goal is to facilitate smooth and effective communication, ensuring the user's needs are met and 
+  the conversation stays on track. 
+  The list is as follows:
+`;
 
 export const chatFunctions: ChatCompletionFunctions[] = [
   {
