f286badfb4acdd916f89015685b595e20b12df6f
Isaac <isaac.castro.dev@gmail.com>
2023-06-29 21:20:23 -0600

    Installed react toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,15 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 // ** ChakraUI
 import { ChakraProvider } from "@chakra-ui/react";
+// ** React Toastify
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <ToastContainer />
     </ChakraProvider>
   );
 }
