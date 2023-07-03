50f73820db4e57e2971b1fb5148aaffdca14fd70
Isaac <isaac.castro.dev@gmail.com>
2023-07-03 11:14:44 -0600

    Added "selectAgent" function to function list

--- src/chat/function.ts
+++ src/chat/function.ts
@@ -2,18 +2,24 @@ import { ChatCompletionFunctions } from "openai";
 
 export enum ChatFunctions {
   runCompletion = "runCompletion",
+  selectAgent = "selectAgent",
 }
 
 export const moderatorDescription = `
-  You're an AI moderator who oversees the interactions between the user and various assistants. 
-  Your primary task is to make a unique conversation between the user and the assistants.
-  Every assistant has a unique personality and will respond differently to the user.
+  You're an AI moderator who oversees the interactions between the user and various Agents.
+  An Agent is an AI that has a unique personality and will respond differently to the user.
+
+  Your primary task is to coordinate the conversation between the user and the agents.
   
-  Given the context of the conversation, you must select an assistant to continue the conversation 
-  or let the user respond. 
+  You will select an agent to respond to the conversation, send a message as that agent, and then 
+  keep moderating the conversation.
+
+  You can select an agent by calling the "selectAgent" function.
+  After a function call selecting an agent, you'll send a message as that agent.
+
+  Do not call a function twice in a row.
   
-  Try to make the conversation as interesting as possible, if there's nothing that an assistant can say,
-  it's okay to let the user respond. You must have invoked the function before letting the user respond.
+  Make the conversation engaging, with multiple agents giving different responses to the user.
 
   You have access to a list of all available agents, their IDs, and descriptions. If the list is empty, you must return a "default" as an id.
   List of agents:
@@ -21,9 +27,11 @@ export const moderatorDescription = `
 
 export const chatFunctions: ChatCompletionFunctions[] = [
   {
-    name: ChatFunctions.runCompletion,
-    description: `Continues the conversation with the selected agent. 
-    If there's no agent selected, the user will be able to respond.`,
+    name: ChatFunctions.selectAgent,
+    description: `Given an agent id, the next message will be sent by the selected agent.
+    It will return the description of the selected agent. Your next answer must follow the description.
+    
+    `,
     parameters: {
       type: "object",
       properties: {
