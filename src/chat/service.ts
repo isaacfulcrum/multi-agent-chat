bee926ba4493fe6a19418aaae7825fcec62f04a8
Isaac <isaac.castro.dev@gmail.com>
2023-07-03 17:12:52 -0600

    Refactor chat service

--- src/chat/service.ts
+++ src/chat/service.ts
@@ -1,13 +1,12 @@
 import { BehaviorSubject, Subscription } from "rxjs";
+import { ChatCompletionRequestMessage } from "openai";
 import { nanoid } from "nanoid";
 
 import { Agent } from "@/agent/type";
 import { agentServiceInstance } from "@/agent/service";
 
-import { fetchAgent, fetchChatCompletionStream } from "./api";
+import { fetchChatCompletionStream } from "./api";
 import { chatMessageToCompletionMessage, AssistantChatMessage, ChatMessage, ChatMessageRoleEnum } from "./type";
-import { ChatFunctions } from "./function";
-import { ChatCompletionRequestMessage } from "openai";
 
 const MAX_CONSECUTIVE_ASSISTANT_MESSAGES = 5;
 
@@ -47,36 +46,13 @@ export class ChatService {
     }
   }
 
-  // == Private Methods ===========================================================
   //** returns the current messages directly from the source */
   private getMessages() {
     return this.messages$.getValue();
   }
 
-  // == Agent =====================================================================
-  /** Semantically chooses an Agent based on a history of messages and the Agent's
-   *  description */
-  private async selectAgent(messages: ChatCompletionRequestMessage[], agents: Agent[]): Promise<Agent | null/*not found*/> {
-    const chatFunction = await fetchAgent(messages, agents);
-    if (!chatFunction) throw new Error("No function call was returned");
-
-    // TODO: Find a better way to invoke the function, for now we just use an if
-    if (chatFunction.name !== ChatFunctions.runCompletion) throw new Error("Invalid function call");
-    const args = JSON.parse(chatFunction.arguments ?? "{}");
-    if (!args.agentId) return null;
-
-    // Check if the last message was sent by the selected agent
-    const lastMessage = this.getMessages().slice(-1)[0];
-    if (lastMessage.role === ChatMessageRoleEnum.Assistant) {
-      if(args.agentId === "default" && !lastMessage.isAgent) return null;
-      if (lastMessage.isAgent && lastMessage.agent?.id === args.agentId) return null;
-    }
-
-    return agentServiceInstance.getAgent(args.agentId) ?? null;
-  };
-
   // == Completion ================================================================
-  /** Select an Agent to respond to the conversation based on the Context and Agent 
+  /** Select an Agent to respond to the conversation based on the Context and Agent
    *  description.
    *  Respond based on the following conditions:
    *  1. The previous {@link MAX_CONSECUTIVE_ASSISTANT_MESSAGES } messages where not
@@ -84,48 +60,51 @@ export class ChatService {
    *  2. An agent cannot respond consecutively. */
   public async requestCompletion() {
     try {
-      if (this.isLoading) return/*nothing else to do*/;
-      this.isLoading = true;
-
+      if (this.isLoading) return /*nothing else to do*/;
       const isConsecutive = this.getMessages()
         .slice(-MAX_CONSECUTIVE_ASSISTANT_MESSAGES)
         .every((message) => message.role === ChatMessageRoleEnum.Assistant);
-      if(isConsecutive) return/* nothing else to do */;
+      if (isConsecutive) return /* nothing else to do */;
 
-      // Format messages as OpenAI expects them
-      const messages = this.getMessages().map(chatMessageToCompletionMessage);
+      const messageHistory = this.getMessages().map(chatMessageToCompletionMessage);
 
-      const selectedAgent = await this.selectAgent(messages, agentServiceInstance.getActiveAgents());
-      if(!selectedAgent) return/*nothing else to do*/;
+      const selectedAgent = await agentServiceInstance.selectAgent(messageHistory);
+      if (!selectedAgent) return /*nothing else to do*/;
 
-      await this.runCompletion(messages, selectedAgent);
+      await this.runCompletion(messageHistory, selectedAgent);
     } catch (error) {
       // TODO: handle error
       console.error("Error:", error);
-    } finally {
-      this.isLoading = false;
-    }
+    } 
   }
 
   /** runs the Completion with the current messages */
   public async runCompletion(messages: ChatCompletionRequestMessage[], agent?: Agent) {
     try {
-      // New message
+      if (this.isLoading) return /*nothing else to do*/;
+      this.isLoading = true;
+
+      const messageHistory = [...messages];
+
+      // -- New Message ------------------------------------------------------------
       const id = nanoid();
       let chatMessage: AssistantChatMessage = { id, role: ChatMessageRoleEnum.Assistant, content: "", isAgent: false };
-      // If the agent exists, we add its description as a system message
-      // NOTE: add to the start of the array so it's the first message
       if (agent) {
-        messages.unshift({ role: ChatMessageRoleEnum.System, content: agent.description });
+        // If the agent exists, we add its description as a system message
+        // NOTE: add to the start of the array so it's the first message
+        const systemMessage = {
+          role: ChatMessageRoleEnum.System,
+          content: agent.description,
+        };
+        messageHistory.unshift(systemMessage);
         // NOTE: Add the agent to the message so we can display it in the UI
         chatMessage = { id, role: ChatMessageRoleEnum.Assistant, content: "", isAgent: true, agent };
       }
 
-      this.isLoading = true;
       this.addMessage(chatMessage);
 
       // Fetch the response from the OpenAI API and update the content of the message
-      const completion$ = await fetchChatCompletionStream(messages);
+      const completion$ = await fetchChatCompletionStream(messageHistory );
       if (!completion$) {
         throw new Error("Empty response");
       }
@@ -136,7 +115,6 @@ export class ChatService {
           this.updateMessage({ ...chatMessage, content });
         },
         complete: () => {
-          // When the stream is completed, evaluate if we need to continue the chat
           this.requestCompletion();
         },
         error: (error) => {
@@ -146,7 +124,9 @@ export class ChatService {
     } catch (error) {
       // TODO: handle error
       console.error(error);
-    } 
+    } finally {
+      this.isLoading = false;
+    }
   }
 
   // == Chat ======================================================================
