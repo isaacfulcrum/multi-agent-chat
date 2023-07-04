c3705177c7c65133f2169927860248ac12cffddc
Isaac <isaac.castro.dev@gmail.com>
2023-07-03 19:58:22 -0600

    Refactor function prompts

--- src/chat/function.ts
+++ src/chat/function.ts
@@ -6,32 +6,25 @@ export enum ChatFunctions {
 }
 
 export const moderatorDescription = `
-  You're an AI moderator who oversees the interactions between the user and various Agents.
-  An Agent is an AI that has a unique personality and will respond differently to the user.
+You're an AI moderator who oversees the interactions between the user and various assistants. 
+Your primary task is to make a unique conversation between the user and the assistants.
+Every assistant has a unique personality and will respond differently to the user.
 
-  Your primary task is to coordinate the conversation between the user and the agents.
-  
-  You will select an agent to respond to the conversation, send a message as that agent, and then 
-  keep moderating the conversation.
+Given the context of the conversation, you must select an assistant to continue the conversation 
+or let the user respond. 
 
-  You can select an agent by calling the "selectAgent" function.
-  After a function call selecting an agent, you'll send a message as that agent.
+Try to make the conversation as interesting as possible, if there's nothing that an assistant can say,
+it's okay to let the user respond. You must have invoked the function before letting the user respond.
 
-  Do not call a function twice in a row.
-  
-  Make the conversation engaging, with multiple agents giving different responses to the user.
-
-  You have access to a list of all available agents, their IDs, and descriptions. If the list is empty, you must return a "default" as an id.
-  List of agents:
+You have access to a list of all available agents, their IDs, and descriptions. If the list is empty, you must return a "default" as an id.
+List of agents:
 `;
 
 export const chatFunctions: ChatCompletionFunctions[] = [
   {
     name: ChatFunctions.selectAgent,
-    description: `Given an agent id, the next message will be sent by the selected agent.
-    It will return the description of the selected agent. Your next answer must follow the description.
     
-    `,
+    description: `Given an agent id, the next message will be sent by the selected agent.`,
     parameters: {
       type: "object",
       properties: {
