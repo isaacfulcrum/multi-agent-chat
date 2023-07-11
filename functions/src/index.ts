e6c14a57fa774b3561574e543475dfb0dded81e2
Isaac <isaac.castro.dev@gmail.com>
2023-07-11 15:29:29 -0600

    First draft of memory storage functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import { onCall } from "firebase-functions/v2/https";
 
 import { initializeApp } from "firebase-admin/app";
@@ -17,16 +8,17 @@ import { getFirestore } from "firebase-admin/firestore";
 
 initializeApp();
 
-exports.addMessage = onCall(async (request) => {
+exports.storeMemories = onCall<{ [key: string]: string }>(async (request) => {
   // Grab the text parameter.
-  const original = request.data.text;
+  const memories = request.data;
   // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await getFirestore().collection("messages").add({ original: original });
-  // Send back a message that we've successfully written the message
-  return { result: `Message with ID: ${writeResult.id} added.` };
+  const concepts = getFirestore().doc("memories/concepts"); /* Create a new document reference */
+  concepts.update({
+    ...memories,
+  });
 });
 
-exports.getMessages = onCall(async (request) => {
-  const messages = await getFirestore().collection("messages").get();
-  return { messages: messages.docs.map((doc) => doc.data()) };
+exports.getMemories = onCall(async (request) => {
+  const memories = await getFirestore().doc("memories/concepts").get();
+  return memories.data();
 });
