39dee630a0d537e7e1aeb38e4ab3a75915f04fe3
Isaac <isaac.castro.dev@gmail.com>
2023-07-19 18:43:57 -0600

    Refactor

--- src/agent/component/AgentCreation.tsx
+++ src/agent/component/AgentCreation.tsx
@@ -8,26 +8,19 @@ import { toast } from "react-toastify";
 // *******************************************************************************
 export const AgentCreation = () => {
   // === State ================================================================
-  const [newAgent, setNewAgent] = useState({
-    name: "",
-    description: "",
-  });
-  const isValid = newAgent.name.trim() !== "" && newAgent.description.trim() !== "";
+  const [agentName, setAgentName] = useState("");
+  const [agentDescription, setAgentDescription] = useState("");
+  const isValid = agentName.trim() !== "" && agentDescription.trim() !== "";
 
   // === Handlers =============================================================
-  const changeNewAgent = (key: "name" | "description", value: string) => setNewAgent({ ...newAgent, [key]: value });
-
-  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => changeNewAgent("name", e.target.value);
-
-  const handleDescriptionChange = (e: ChangeEvent<HTMLTextAreaElement>) =>
-    changeNewAgent("description", e.target.value);
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => setAgentName(e.target.value);
+  const handleDescriptionChange = (e: ChangeEvent<HTMLTextAreaElement>) => setAgentDescription(e.target.value);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const color = getRandomHex();
     try {
-      await agentServiceInstance.newAgent({ ...newAgent, color });
-      setNewAgent({ name: "", description: "" });
+      await agentServiceInstance.newAgent({ name: agentName, description: agentDescription, color });
       toast.success("Agent created successfully");
     } catch (error) {
       console.log(error);
@@ -44,8 +37,8 @@ export const AgentCreation = () => {
       </Box>
       <form onSubmit={handleSubmit}>
         <Stack spacing="4">
-          <Input placeholder="Name" value={newAgent.name} onChange={handleNameChange} backgroundColor="#40414f" />
-          <Textarea placeholder="Description" value={newAgent.description} onChange={handleDescriptionChange} backgroundColor="#40414f" />
+          <Input placeholder="Name" value={agentName} onChange={handleNameChange} backgroundColor="#40414f" />
+          <Textarea placeholder="Description" value={agentDescription} onChange={handleDescriptionChange} backgroundColor="#40414f" />
           <Button colorScheme="teal" variant="solid" type="submit" isDisabled={!isValid}>
             Create
           </Button>
