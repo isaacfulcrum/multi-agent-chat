55c66b255a6c2f04fdb989421daf5477fe2eceae
Isaac <isaac.castro.dev@gmail.com>
2023-08-03 12:16:21 -0600

    Complete functionality automatic completion chat

--- src/chat/automatic.ts
+++ src/chat/automatic.ts
@@ -1,10 +1,13 @@
 import { AgentIdentifier } from "@/agent/type";
-import { AbstractChatService, ChatMessage } from "./type";
 import { AgentControllerService } from "@/agentController/service";
-import { ChatFunctions, chatFunctions, getModeratorPrompt } from "./function";
 import { OpenAIService } from "@/openai/service";
 import { ConversationalAgentOpenAI } from "@/agent/service";
+
+import { AbstractChatService, ChatMessage, ChatMessageRole } from "./type";
 import { chatMessagesToCompletionMessages, createAgentMessage } from "./util";
+import { ChatFunctions, chatFunctions, getModeratorPrompt } from "./function";
+
+const MAX_CONSECUTIVE_ASSISTANT_MESSAGES = 5;
 
 /**
  * TODO: This is a temporary file for the automatic agent chat
@@ -14,20 +17,12 @@ import { chatMessagesToCompletionMessages, createAgentMessage } from "./util";
  * to respond to the user*/
 // ********************************************************************************
 export class AutomaticAgentChat extends AbstractChatService {
-  // == Singleton =================================================================
-  private static singleton: AutomaticAgentChat;
-  public static getInstance() {
-    if (!AutomaticAgentChat.singleton) AutomaticAgentChat.singleton = new AutomaticAgentChat();
-    return AutomaticAgentChat.singleton;
-  }
-
   // == Lifecycle =================================================================
   protected constructor() {
     super();
   }
 
   // == Completion ================================================================
-
   /** Semantically chooses an Agent based on a history of messages and the Agent's
    *  description */
   public async selectAgent(messages: ChatMessage[]): Promise<AgentIdentifier | null /*not found*/> {
@@ -69,20 +64,37 @@ export class AutomaticAgentChat extends AbstractChatService {
   }
 
   /** Single agent completion request, in this mode the selected agent in XXX will
-   * be the one responding*/
-  public requestCompletion = async () => {
+   * be the one responding
+   * *  Respond based on the following conditions:
+   *  1. The previous {@link MAX_CONSECUTIVE_ASSISTANT_MESSAGES } messages where not
+   *     sent exclusively by the assistant.
+   *  2. An agent cannot respond consecutively. */
+  public async requestCompletion() {
     try {
-      const agentId = await this.selectAgent(this.getMessages());
+      if (this.isLoading) throw new Error("Another completion is already in progress.");
+      const messages = this.getMessages();
 
-      const agent = new ConversationalAgentOpenAI("1");
+      // -----------------------------------------------------------------------
+      const isConsecutive = messages.slice(-MAX_CONSECUTIVE_ASSISTANT_MESSAGES).every((message) => message.role === ChatMessageRole.Assistant);
+      if (isConsecutive) return;
+
+      const agentId = await this.selectAgent(messages);
+      if (!agentId) throw new Error("No agent selected");
+
+      const lastMessage = messages[messages.length - 1];
+      const alreadyResponded = lastMessage.role === ChatMessageRole.Assistant && lastMessage.agent.id === agentId;
+      if (alreadyResponded) return;
+
+      const agent = new ConversationalAgentOpenAI(agentId);
       const AgentProfile = await agent.getProfile();
       if (!AgentProfile) throw new Error(`Agent ${agent} not found`); // TODO: maybe this should be handled by getProfile()
 
+      // -----------------------------------------------------------------------
       /*create the message*/
       const message = createAgentMessage("", AgentProfile);
       this.isLoading = true;
       let messageAdded = false; /*add the message only when we have an actual response*/
-      await agent.getResponse(chatMessagesToCompletionMessages(this.getMessages()), (content) => {
+      await agent.getResponse(chatMessagesToCompletionMessages(messages), (content) => {
         if (!messageAdded) {
           this.addMessage({ ...message, content });
           messageAdded = true;
@@ -91,10 +103,12 @@ export class AutomaticAgentChat extends AbstractChatService {
         this.updateMessage({ ...message, content });
       });
       this.isLoading = false;
+
+      // Request another completion
+      this.requestCompletion();
     } catch (error) {
       console.error(error);
       // Handle error
-    } finally {
     }
-  };
+  }
 }
