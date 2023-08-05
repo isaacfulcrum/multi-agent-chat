416419503921292f295ffc69be70c3e5609463d4
Isaac <isaac.castro.dev@gmail.com>
2023-08-04 23:51:05 -0600

    Consistency: Agent routes match services names

--- src/pages/interactive.tsx
+++ src/pages/interactive.tsx
@@ -1,12 +1,11 @@
 import { Chat } from "@/chat/component";
-import { IterativeAgentChat } from "@/chat/service/iterative";
+import { AutomaticAgentChat } from "@/chat/service";
 import { ChatProviderComponent } from "@/chat/context/ChatProviderComponent";
 
-
-const chat = new IterativeAgentChat();
+const chat = new AutomaticAgentChat();
 
 //**********************************************************************************
-export default function AutomaticChatPage() {
+export default function InteractiveChatPage() {
   return (
     <ChatProviderComponent chat={chat}>
       <Chat />
--- src/pages/iterative.tsx
+++ src/pages/iterative.tsx
@@ -1,12 +1,11 @@
 import { Chat } from "@/chat/component";
-import { AutomaticAgentChat } from "@/chat/service";
+import { IterativeAgentChat } from "@/chat/service/iterative";
 import { ChatProviderComponent } from "@/chat/context/ChatProviderComponent";
 
-
-const chat = new AutomaticAgentChat();
+const chat = new IterativeAgentChat();
 
 //**********************************************************************************
-export default function AutomaticChatPage() {
+export default function IterativeChatPage() {
   return (
     <ChatProviderComponent chat={chat}>
       <Chat />
